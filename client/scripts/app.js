0702f1603791f572936f94e22f574a1d25783b86
concreted <arichuang@gmail.com>
2014-09-22 16:21:54 -0700

    Displaying messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,91 @@
 // YOUR CODE HERE:
+var app = {
+  server:'https://api.parse.com/1/classes/chatterbox',
+  init: function(){
+    setInterval(function() {
+      console.log('running');
+      this.fetch();
+
+    }, 5000);
+  },
+  send: function(message){
+    $.ajax({
+      // always use this url
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      // always use this url
+      url: this.server,
+      type: 'GET',
+      // data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+        console.log('chatterbox: Message fetched');
+        app.clearMessages();
+        _.each(data['results'], (function(message) {
+          console.log(this);
+          this.addMessage(message);
+        }).bind(app));
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message');
+      }
+    });
+  },
+  clearMessages: function(){
+    $('#chats').empty();
+  },
+  addMessage: function(message) {
+    $username = $('<span class="username">').text(message.username);
+    $text = $('<span class="text">').text(': ' + message.text);
+    $full = $('<div class="message">').append($username).append($text)
+    $('#chats').append($full);
+  },
+  addRoom: function(room) {
+    $('#roomSelect').append('<span class="roomName">' + room + '</span>');
+  },
+  addFriend: function(friend) {
+    console.log(friend);
+  },
+  handleSubmit: function(message) {
+    console.log('Submit', message);
+    var user = window.location.search.slice(10);
+
+    var messageObject = {
+      username: user,
+      text: message,
+      roomname: null
+    };
+
+    console.log(messageObject);
+
+    app.send(messageObject);
+  }
+
+};
+
+$(document).ready(function() {
+  $("#chats").on('click', '.username', function() {
+    app.addFriend($(this).text());
+  });
+  $('#send .submit').click(function(event) {
+    app.handleSubmit($('.chatbox').val());
+  });
+});
+
+
 
